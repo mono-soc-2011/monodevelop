5654f8a3908608a474ac0df9f2e687a22543717c
Krzysztof Marecki <marecki.krzysztof@gmail.com>
2011-05-03 12:06:43 +0200

    Merge MonoDevelop.GtkCore.GuiBuilder/GuiBuilderService.cs
    Port revision Michael Hutchinson 1/7/2011 11:02:06 AM
    Overhaul the code formatter extension points

--- main/src/addins/MonoDevelop.GtkCore2/MonoDevelop.GtkCore.GuiBuilder/GuiBuilderService.cs
+++ main/src/addins/MonoDevelop.GtkCore2/MonoDevelop.GtkCore.GuiBuilder/GuiBuilderService.cs
@@ -542,7 +542,7 @@ namespace MonoDevelop.GtkCore.GuiBuilder
 			//TODO : Wait for a fix for Mono.TextEditor.Document in the trunk
 			//content = StripHeaderAndBlankLines (content, provider);
 			string mt = DesktopService.GetMimeTypeForUri (file);
-			Formatter formatter = TextFileService.GetFormatter (mt);
+			var formatter = MonoDevelop.Ide.CodeFormatting.CodeFormatterService.GetFormatter (mt);
 			if (formatter != null)
 				content = formatter.FormatText (PolicyService.InvariantPolicies, content);
 			return content;
