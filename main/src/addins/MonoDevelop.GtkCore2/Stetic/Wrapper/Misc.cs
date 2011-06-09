f0bfc959d2d418762dbf4eb29bd168308f10c039
Krzysztof Marecki <marecki.krzysztof@gmail.com>
2011-06-09 23:52:24 +0200

    Add property for setting horizontal aligment in the properties pad

--- main/src/addins/MonoDevelop.GtkCore2/Stetic/Wrapper/Misc.cs
+++ main/src/addins/MonoDevelop.GtkCore2/Stetic/Wrapper/Misc.cs
@@ -3,8 +3,38 @@ using System;
 
 namespace Stetic.Wrapper
 {
+	public enum HorizontalAlignments 
+	{
+		AlignLeft,
+		AlignCenter,
+		AlignRight,
+		None
+	}
+	
 	public class Misc: Widget
 	{
+		public HorizontalAlignments Alignment {
+			get {
+				if (AlignLeft) {
+					return HorizontalAlignments.AlignLeft;
+				}
+				if (AlignRight) {
+					return HorizontalAlignments.AlignRight;
+				}
+				if (AlignCenter) {
+					return HorizontalAlignments.AlignCenter;
+				}
+				return HorizontalAlignments.None;
+			}
+			set {
+				switch (value) {
+				case HorizontalAlignments.AlignLeft : AlignLeft = true; break;
+				case HorizontalAlignments.AlignRight : AlignRight = true; break;
+				case HorizontalAlignments.AlignCenter : AlignCenter = true; break;
+				}
+			}
+		}
+		
 		public bool AlignLeft {
 			get {
 				return ((Gtk.Misc)Wrapped).Xalign == 0;
